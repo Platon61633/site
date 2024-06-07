4d75e517eefb0fb36073e73b95f494ebf96f06e4
fname0 <133584066+fname0@users.noreply.github.com>
2024-06-07 13:09:05 +0000

    luchsii_commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="porno">
-      порно без регистрации =
+      порно без регистрации = класс
     </div>
   );
 }
