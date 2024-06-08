6dbd8fd9111037d8bf57d2b88fcc73f7958b8c08
Platon61633 <peredreevplaton@gmail.com>
2024-06-08 12:27:36 +0300

    c

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
       return;
     }
     setIsLoading(true);
-    axios.post('https://zxctmol-dfzbx4rf9-spo0dys-projects.vercel.app/upload_csv', {data: file}).then(e=>console.log(e.data))
+    axios.post('https://zxctmol-g7rfg62il-spo0dys-projects.vercel.app/upload_csv', {data: file}).then(e=>console.log(e.data))
     console.log(file);
     setIsLoading(false)
     // const reader = new FileReader();
